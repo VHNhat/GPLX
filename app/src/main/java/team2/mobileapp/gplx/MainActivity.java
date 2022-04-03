c3f295df9c42b85ef210f30969a3260b760896df
Bùi Quốc Huy <huy01682312920@gmail.com>
2022-04-03 10:02:20 +0700

    layout_welcome_lazy_signin_register

--- app/src/main/java/team2/mobileapp/gplx/MainActivity.java
+++ app/src/main/java/team2/mobileapp/gplx/MainActivity.java
@@ -2,7 +2,9 @@ package team2.mobileapp.gplx;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public  void Lazy ( View view)
+    {
+        Intent v = new Intent(this,LazyActivity.class);
+        startActivity(v);
+
+    }
+    public  void Register ( View view)
+    {
+        Intent v = new Intent(this,RegisterActivity.class);
+        startActivity(v);
+
+    }
+    public  void SignIn ( View view)
+    {
+        Intent v = new Intent(this,SignInActivity.class);
+        startActivity(v);
+
+    }
+    public  void Welcome ( View view)
+    {
+        Intent v = new Intent(this,WelcomeActivity.class);
+        startActivity(v);
+
+    }
+
 }
