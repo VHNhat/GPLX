b4cf8bce66c8f9c37fc87d0f573e044abbf65096
Bùi Quốc Huy <huy01682312920@gmail.com>
2022-06-06 16:29:49 +0700

    checkpoint

--- API/GPLX_API/src/main/java/team2/api/mobile/gplx/service/HistoricalExamServiceImpl.java
+++ API/GPLX_API/src/main/java/team2/api/mobile/gplx/service/HistoricalExamServiceImpl.java
@@ -1,9 +1,11 @@
 package team2.api.mobile.gplx.service;
 
+import org.springframework.stereotype.Service;
+
 import team2.api.mobile.gplx.commondata.GenericServiceImpl;
 import team2.api.mobile.gplx.models.HistoricalExam;
 import team2.api.mobile.gplx.service.interfaces.HistoricalExcemService;
-
-public class HistoricalExamServiceImpl extends GenericServiceImpl<HistoricalExam, String> implements HistoricalExcemService {
+@Service
+ public class HistoricalExamServiceImpl extends GenericServiceImpl<HistoricalExam, String> implements HistoricalExcemService {
 
 }
